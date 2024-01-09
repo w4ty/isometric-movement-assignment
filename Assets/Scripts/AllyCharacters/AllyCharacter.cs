126fd27e575641ae30ad9a7a15331ba3f631f2ae
w4ty <therex102@gmail.com>
2024-01-09 13:27:32 +0100

    feat: add AllyCharacter stat randomization
    Randomization uses CharacterData scriptable object to get min max values

--- Assets/Scripts/AllyCharacters/AllyCharacter.cs
+++ Assets/Scripts/AllyCharacters/AllyCharacter.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class AllyCharacter : MonoBehaviour, IMoveable, IKillable
 {
+    public CharacterDataScriptableObject CharacterData;
+
     public float Speed { get; set; }
     public float Agility { get; set; }
     public float Health { get; set; }
+
+    void Awake()
+    {
+        Speed = Random.Range(CharacterData.SpeedMinMax.x, CharacterData.SpeedMinMax.y);
+        Agility = Random.Range(CharacterData.AgilityMinMax.x, CharacterData.AgilityMinMax.y);
+        Health = Mathf.RoundToInt(Random.Range(CharacterData.HealthMinMax.x, CharacterData.HealthMinMax.y));
+        Debug.Log(Speed + "\n" + Agility + "\n" + Health);
+    }
 
     public void Move(Vector3 position)
     {
