8f75ab42312fe3d008c4fa6c0b5bcf7820b66b82
w4ty <therex102@gmail.com>
2024-01-09 21:36:54 +0100

    feat: implement ISelectable for AllyCharacter

--- Assets/Scripts/AllyCharacters/AllyCharacter.cs
+++ Assets/Scripts/AllyCharacters/AllyCharacter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AllyCharacter : MonoBehaviour, IMoveable, IKillable
+public class AllyCharacter : MonoBehaviour, IMoveable, IKillable, ISelectable
 {
     public CharacterDataScriptableObject CharacterData;
 
