0744b8a9b95947e4f2d19a469ef68e9ea66cae5c
w4ty <therex102@gmail.com>
2024-01-09 21:18:38 +0100

    feat: load map size and char amount from GameData

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,16 +6,19 @@ public class GameManager : MonoBehaviour
 {
     private int _allyCharacterAmount;
     private Vector3 _mapSize;
+    public GameDataScriptableObject GameData;
     public Terrain BaseTerrain;
     public AllyCharacter Character;
 
     void CreateMap()
     {
+        _mapSize = GameData.MapSize;
         BaseTerrain.enabled = true;
         BaseTerrain.terrainData.size = _mapSize;
     }
     void CreateCharacters()
     {
+        _allyCharacterAmount = GameData.AllyCharacterAmount;
         Debug.Log("Creating characters");
     }
 }
