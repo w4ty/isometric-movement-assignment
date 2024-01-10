5f3c456c3b5cf0cec227908682e64531b789fed8
w4ty <therex102@gmail.com>
2024-01-10 12:50:24 +0100

    feat: set GameManager to listen to events
    GameManager listens to the OnExited event from InputManager to know when to quit the program

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private InputManager inputManager;
     private int _allyCharacterAmount;
     private Vector3 _mapSize;
     public GameDataScriptableObject GameData;
     public Terrain BaseTerrain;
     public AllyCharacter Character;
 
+    private void OnExited()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+
     void Awake()
     {
         CreateMap();
+        if (inputManager != null)
+        {
+            inputManager.OnExited += OnExited;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (inputManager != null) 
+        {
+            inputManager.OnExited -= OnExited;
+        }
     }
 
     void CreateMap()
