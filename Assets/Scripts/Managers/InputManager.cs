faa8d476f6714af64cfeb616818b766722f4e17c
w4ty <therex102@gmail.com>
2024-01-10 12:18:58 +0100

    feat: create InputManager script

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    void OnExit()
+    {
+
+    }
+    void OnSelect()
+    {
+
+    }
+    void OnInteract()
+    {
+
+    }
+}
