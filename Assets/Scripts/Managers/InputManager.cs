e51561a9355466699980dcdb3f2f60fc1ef6328b
w4ty <therex102@gmail.com>
2024-01-10 12:32:34 +0100

    feat: add events to InputManager
    InputManager has events for left click (Selected), right click (Interacted) and escape (Exited)

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,19 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public event Action OnExited;
+    public event Action<Vector3> OnSelected;
+    public event Action<Vector3> OnInteracted;
     void OnExit()
     {
-
+        OnExited?.Invoke();
     }
     void OnSelect()
     {
-
+        OnSelected?.Invoke(Input.mousePosition);
     }
     void OnInteract()
     {
-
+        OnInteracted?.Invoke(Input.mousePosition);
     }
 }
