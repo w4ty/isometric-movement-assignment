d7e345c670845465a3efcebb00c7cfd57ffaf789
w4ty <therex102@gmail.com>
2024-01-12 12:27:59 +0100

    refactor: use SpawnerInfo in GameData scriptable

--- Assets/Scripts/ScriptableObjects/GameDataScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/GameDataScriptableObject.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GameDataScriptableObject", order = 1)]
 public class GameDataScriptableObject : ScriptableObject
 {
-    public List<Vector3> AllyCharacters = new List<Vector3>();
     public Vector3 MapSize = new Vector3(50, 50, 600);
+    public List<SpawnerInfo> Spawns = new List<SpawnerInfo>();
 }
