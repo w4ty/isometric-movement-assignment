8ea1dd88aba76757f7f272e33e16432a1c93808e
w4ty <therex102@gmail.com>
2024-01-12 12:15:52 +0100

    feat: create SpawnerInfo script
    Used for GameData scriptable to indicate what object should be spawned where at runtime

---
+++ Assets/Scripts/Helpers/SpawnerInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[Serializable]
+public class SpawnerInfo
+{
+    public Vector3 Position;
+    public GameObject SpawnObject;
+}
