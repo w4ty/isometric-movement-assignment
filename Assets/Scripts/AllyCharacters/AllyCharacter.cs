2b430e6bfdbc3087a95244fc499aabca0491d494
w4ty <therex102@gmail.com>
2024-01-13 16:20:29 +0100

    chore: remove unused debug log

--- Assets/Scripts/AllyCharacters/AllyCharacter.cs
+++ Assets/Scripts/AllyCharacters/AllyCharacter.cs
@@ -15,7 +15,6 @@ public class AllyCharacter : MonoBehaviour, IMoveable, IKillable, ISelectable, I
         Speed = Random.Range(CharacterData.SpeedMinMax.x, CharacterData.SpeedMinMax.y);
         Agility = Random.Range(CharacterData.AgilityMinMax.x, CharacterData.AgilityMinMax.y);
         Health = Mathf.RoundToInt(Random.Range(CharacterData.HealthMinMax.x, CharacterData.HealthMinMax.y));
-        Debug.Log(Speed + "\n" + Agility + "\n" + Health);
     }
 
     public void Move(Vector3 position)
